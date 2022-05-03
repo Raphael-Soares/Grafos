8660703ec0eaced48c4261db7ac245b34acb6ee0
Raphael-Soares <soaresphaell@gmail.com>
2022-05-03 14:06:13 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <loginForm/>
+</template>
+
+<script>
+import loginForm from './components/loginForm.vue'
+export default {
+  components: {
+    loginForm
+},
+data(){
+  return {
+  
+  }
+}
+
+}
+</script>
+
+<style>
+
+</style>
